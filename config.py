8ab30fb86d4c1cff5194a0e5b2f385907c26fe3c
Max00355 <max00355@gmail.com>
2014-03-02 23:08:39 -0500

    Changed config settings to stock

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import landerdb
 my_data = landerdb.Connect("data.db")
 nodes = landerdb.Connect("nodes.db")
 cache = landerdb.Connect("cache.db") # This is the local data such as messages that aren't held on the network itself.
-master_dns = "localhost"
-host = "localhost"
+master_dns = "sperre.zapto.org"
+host = "0.0.0.0"
 port = 7070
 conversation_servers = {} # address:server Used by the nodes that are looking to send messages in an already open conversation
-relay = True
+relay = False
 found_convo = False
 relaying_to = [] # For relay nodes only, this is a list of active connections that the node is proxying messages to.
